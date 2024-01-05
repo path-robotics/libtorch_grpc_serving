011af2d9cceb7ab16d71cb5ea9a94cd668da7cca
Peter Jochem <pjochem@path-robotics.com>
2024-01-05 17:23:24 -0500

    Adds master install script.

---
+++ install_and_build.sh
@@ -0,0 +1,12 @@
+
+./install_apt_packages.sh 
+./get_go.sh
+./get_grpc.sh
+./install_zlib.sh
+./install_cares.sh
+./install_protobuf.sh
+./install_grpc.sh
+./install_libtorch.sh
+source setup_python.sh
+./compile_protos.sh
+./build_server.sh
